045bf53563e0cf13f0b5aeb4e2890f19f2420bc1
IremBalıkcı <irembalikciii98@gmail.com>
2025-05-19 17:01:46 +0300

    veri tabanındaki veriler ana sayfaya aktarıldı.

--- my_todo_app/lib/ui/screens/main_screen.dart
+++ my_todo_app/lib/ui/screens/main_screen.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_todo_app/data/entity/todos.dart';
 import 'package:my_todo_app/ui/components/my_app_bar.dart';
+import 'package:my_todo_app/ui/cubits/main_cubit.dart';
+import 'package:my_todo_app/ui/tools/app_color.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -9,11 +14,82 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<MainCubit>().loadToDos();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(title: "ToDo"),
-      body: Center(),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CupertinoSearchTextField(
+                placeholder: "Search",
+                onChanged: (searchText) {
+                  context.read<MainCubit>().search(searchText);
+                },
+              ),
+            ),
+            BlocBuilder<MainCubit,List<ToDos>>(
+              builder: (context,toDosList){
+                if (toDosList.isNotEmpty){
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: toDosList.length,
+                      itemBuilder: (context, index) {
+                        var toDo = toDosList[index];
+                        return GestureDetector(
+                          onTap: () {
+                            
+                          },
+                          child: Card(
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: Icon(Icons.collections_bookmark),
+                                ),
+                                Column(
+                                  children: [
+                                    Text(toDo.name),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 20),
+                                      child: Text(toDo.details),
+                                    ),
+                                  ],
+                                ), 
+                                const Spacer(),
+                                IconButton(onPressed: (){
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text("Do you want to delete ${toDo.name}?",
+                                      style: TextStyle(color: textColor2),),
+                                      action: SnackBarAction(label: "Yes",textColor: textColor2, onPressed: (){
+                                        context.read<MainCubit>().delete(toDo.id);
+                                      }),)
+                                  );
+                                }, icon: Icon(Icons.close))
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ));
+                    
+                }else {
+                  return const Center();
+                }
+              },)
+          ],
+        ),
+      ),
     );
   }
 }
